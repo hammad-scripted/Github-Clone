052090eac565ea55a92497515d2141ade3f1def3
Mohammad Hammad <hammad.scripted@gmail.com>
2026-08-20 11:30:05 +0530

    added routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,20 @@
-export default function App() {
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline bg-slate-500">
-      Hello world!
-    </h1>
-  )
-}
+    <div className="flex">
+      <Sidebar />
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/signup" element={<SignupPage />}></Route>
+          <Route path="/explore" element={<ExplorePage />}></Route>
+          <Route path="/likes" element={<LikesPage />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default App;
