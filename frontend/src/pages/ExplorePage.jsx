38618ef89b28e48e8f63d6f90fdc7937c49efd8e
Mohammad Hammad <hammad.scripted@gmail.com>
2026-08-20 12:39:09 +0530

    completed different pages

--- frontend/src/pages/ExplorePage.jsx
+++ frontend/src/pages/ExplorePage.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from 'react';
 
 const ExplorePage = () => {
+  const exploreRepos = (type) => {
+    console.log(type);
+  };
+  const repos=[{
+    name:"React",
+    description:"A JavaScript library for building user interfaces",
+    link:"https://reactjs.org/"
+  }]
   return (
-    <div>ExplorePage</div>
-  )
-}
+    <div className="px-4">
+      <div className="bg-glass max-w-2xl mx-auto rounded-md p-4">
+        <h1 className="text-xl font-bold text-center">
+          Explore Popular Repositories
+        </h1>
+        <div className="flex flex-wrap gap-2 my-2 justify-center">
+          <img
+            src="/javascript.svg"
+            alt="JavaScript ogo"
+            className="h-11 sm:h-20 cursor-pointer"
+            onClick={() => exploreRepos('javascript')}
+          />
+          <img
+            src="/typescript.svg"
+            alt="TypeScript logo"
+            className="h-11 sm:h-20 cursor-pointer"
+            onClick={() => exploreRepos('typescript')}
+          />
+          <img
+            src="/c++.svg"
+            alt="C++ logo"
+            className="h-11 sm:h-20 cursor-pointer"
+            onClick={() => exploreRepos('c++')}
+          />
+          <img
+            src="/python.svg"
+            alt="Python logo"
+            className="h-11 sm:h-20 cursor-pointer"
+            onClick={() => exploreRepos('python')}
+          />
+          <img
+            src="/java.svg"
+            alt="Java logo"
+            className="h-11 sm:h-20 cursor-pointer"
+            onClick={() => exploreRepos('java')}
+          />
+        </div>
+    
+      </div>
+    </div>
+  );
+};
 
-export default ExplorePage
+export default ExplorePage;
